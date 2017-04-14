0c4d3141e07d657a658b431aaf067459dff778e8
w0ldi <woldi41@gmail.com>
2017-04-14 13:29:38 +0200

    warunek if dodany

---
+++ Udemy/IfStatement/script.js
@@ -0,0 +1,10 @@
+var score = 12, players = 6;
+
+	if (score === 12 && players === 6){
+		
+		console.log('Warunek jest prawdziwy')
+		
+		} else {
+			
+			console.log('Warunek jest nieprwadziwy')
+		}
